bd87d4feb9d36dbc7f251df472a98eebeb32d1d3
Miroslaw Biernacki <mb100773@gmail.com>
2022-03-11 20:15:14 +0100

    Add API

--- forms.py
+++ forms.py
@@ -2,12 +2,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField
 from wtforms import validators
+from wtforms.fields.numeric import IntegerField
 from wtforms.validators import DataRequired
 
 
 class TodoForm(FlaskForm):
     title = StringField('Tytuł', validators=[DataRequired()])
-    description = TextAreaField('Opis', validators=[DataRequired()])
+    author = StringField('Autor')
+    description = TextAreaField('Opis')
     done = BooleanField('Czy przeczytana?')
 
 
